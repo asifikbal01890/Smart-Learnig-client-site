48393baf3718007c5f968b4edc8c744950a09362
asifilbal01890 <asifikbal01890@gmail.com>
2022-10-25 11:06:06 +0600

    header complate

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
+
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './App.css';
+import Blog from './Component/Blog/Blog';
+import Courses from './Component/Courses/Courses';
+import Faq from './Component/Faq/Faq';
+import Main from './Layout/Main/Main';
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path:"/",
+      element:<Main></Main>,
+      children:[
+        {
+          path:"/Courses",
+          element: <Courses></Courses>
+        },
+        {
+          path:"/FAQ",
+          element: <Faq></Faq>
+        },
+        {
+          path:"/Blog",
+          element: <Blog></Blog>
+        },
+      ]
+    }
+  ])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <RouterProvider router={router}></RouterProvider>
     </div>
   );
 }
---
+++ src/Component/Header/Header.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Header.css';
+
+const Header = () => {
+    return (
+        <div>
+            <nav className="navbar navbar-expand-lg navbar-dark bg-orange">
+              <div className="container-fluid">
+                <button className="navbar-toggler text-white" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon text-white"></span>
+                </button>
+                <Link className="navbar-brand text-white pl-50" to={'/'}>
+                    <div className='d-flex'>
+                        <div>
+                            <img className='w-10 rounded-circle' src="https://cdn.dribbble.com/users/1405795/screenshots/3979001/media/cac83e63d41f97b41160d01c2b5602c9.jpg?compress=1&resize=800x600&vertical=top" alt=""/>
+                        </div>
+                        <div className='ps-2 fw-bold fs-3'>
+                            Smart Learning
+                        </div>
+                    </div>
+                </Link>
+                <button className='btn btn-light me-50 fw-bolder fs-5 ms-3 d-lg-none'>Login</button>
+                <div className="collapse navbar-collapse" id="navbarTogglerDemo03">
+                    <ul className="navbar-nav me-auto mb-2 mb-lg-0 w-50 m-auto">
+                        <li className="nav-item">
+                        <Link className="nav-link active text-white fs-5" aria-current="page" to={'/Courses'}>Courses</Link>
+                        </li>
+                        <li className="nav-item">
+                        <Link className="nav-link text-white fs-5" to={'/Blog'}>Blog</Link>
+                        </li>
+                        <li className="nav-item">
+                        <Link className="nav-link text-white fs-5" to={'/FAQ'}>FAQ</Link>
+                        </li>
+                        <li className="nav-item">
+                        <Link className="nav-link text-white fs-5" to={'/Blog'}>Blog</Link>
+                        </li>
+                    </ul>
+                    <button type="button" class="btn btn-outline-light fw-bolder fs-5">Dark</button>
+                   
+                </div>
+                <button className='btn btn-light me-50 fw-bolder fs-5 ms-3 d-none d-lg-block'>Login</button>
+              </div>
+            </nav>
+        </div>
+    );
+};
+
+export default Header;
